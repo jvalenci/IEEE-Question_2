867c5db394b034493634a886c7b0242ca64f4a5b
Jonathan Valencia <jonathan.valencia716@gmail.com>
2015-05-14 23:29:20 -0400

    Added method to find distance and if a block is truely blocked

--- IEEE.java
+++ IEEE.java
@@ -7,55 +7,91 @@
 */
 
 import java.util.*;
+import java.io.*;
 
 public class IEEE{
-	public static void main(String[] argv){
+			
+			int n = 0, x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;	//declare variables
+			double c1= 0.0,c2 = 0.0, distance= 0.0;
 
-		Scanner scan = new Scanner(argv[0]);  	//scanner
-		int k = scan.nextInt();		//gets our count variable
-		
-		for(int i = 0; i < k; ++i){
-		
-			int n, x1, y1, z1, x2, y2, z2;
-			double c1,c2, distance;
+	public static void main(String[] argv) throws FileNotFoundException{
 
-			n = scan.nextInt();
-			x1 = scan.nextInt();
-			y1 = scan.nextInt();
-			z1 = scan.nextInt();
-			x2 = scan.nextInt();
-			y2 = scan.nextInt();
-			z2 = scan.nextInt();
+		File file = new File(argv[0]);		//this is my file that is cmd line
 
-			//check to see if the blocks are the same if so return 0
-			if( x1 == x2 && y1 == y2 && z1 == y2){
-				c1 = 0;
-				c2 = 0;
-				System.out.println("This is the same block. The distance is: " + c1 + "With blocked paths. " + c2 + " With out paths ");
-				break;
+		try{
+
+			Scanner scan = new Scanner(file);  	//scanner
+			int k = scan.nextInt();		//gets our count variable
+			
+
+			IEEE ie = new IEEE();	//instantiation
+			
+
+			
+			
+			//this is going to loop the same numbe of times as our
+			//loop count which is the first number in the txt file.
+
+			for(int i = 0; i < k; ++i){
+				
+				ie.n = scan.nextInt();
+				ie.x1 = scan.nextInt();
+				ie.y1 = scan.nextInt();
+				ie.z1 = scan.nextInt();
+				ie.x2 = scan.nextInt();
+				ie.y2 = scan.nextInt();
+				ie.z2 = scan.nextInt();
+					
+				//check to see if the blocks are the same if so return 0
+				if( ie.x1 == ie.x2 && ie.y1 == ie.y2 && ie.z1 == ie.y2){
+					ie.c1 = 0;
+					ie.c2 = 0;
+					System.out.println("This is the same block. The distance is: " + ie.c1 + "With blocked paths. " + ie.c2 + " With out paths ");
+					break;
+				}
+
+				
 			}
-
-			//while findDist has a value it will keep trying to move toward the end
-			while (findDist()){
-
-			}
-
-
-
+			scan.close();
+		
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
 		}
 
 	}//end of main
 
-	public findDist(int x1, int y1, int z1, int x2, int y2, int z2)
+	//finds the distance between two points in space.
+	public double findDist(int x1, int y1, int z1, int x2, int y2, int z2)
 	{
-		if(x2 % 2 == 1 && y2 % 2 == 1 && z2 % 2 == 1){
-			System.out.println("The next block you are trying to reach is blocked")
+		return Math.sqrt( (x1 * x1) - (2 * (x1 * x2)) + (x2 * x2) +
+					      (y1 * y1) - (2 * (y1 * y2)) + (y2 * y2) +
+					      (z1 * z1) - (2 * (z1 * z2)) + (z2 * z2) );
+		
+	}//end of findDist
+
+	//checks if block is blocked
+	public boolean isBlocked(int x1, int y1, int z1)
+	{
+		//check to see if x,y,z are odd
+		if(x1 % 2 == 1 && y1 % 2 == 1 && z1 % 2 == 1){
+			System.out.println("The next block you are trying to reach is blocked.");
+			return true;
 		}else{
-			Math.sqrt( (x1 * x1) - (2 * (x1 * x2)) + (x2 * x2) +
-					   (y1 * y1) - (2 * (y1 * y2)) + (y2 * y2) +
-					   (z1 * z1) - (2 * (z1 * z2)) + (z2 * z2) );
+			return false;
 		}
+	}//end of isBlocked
+
+	public void moveUp(int x, int y, int z)
+	{
+		++z;
+		
 	}
 
 
+
 }//end of IEEE
+
+
+
+
+
